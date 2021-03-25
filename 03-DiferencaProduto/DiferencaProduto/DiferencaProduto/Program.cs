5f6b1fa96c7c17f6b92017ff10149c8625338d1d
Luis Vinicius Pelissoli <vinicius.pelissoli@gmail.com>
2021-03-25 13:34:46 -0300

    Calterei a variavel a para x

--- 03-DiferencaProduto/DiferencaProduto/DiferencaProduto/Program.cs
+++ 03-DiferencaProduto/DiferencaProduto/DiferencaProduto/Program.cs
@@ -9,14 +9,14 @@ namespace DiferencaProduto
             /*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
             de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
 
-            int a, b, c, d, diferenca;
+            int x, b, c, d, diferenca;
 
-            a = int.Parse(Console.ReadLine());
+            x = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
             c = int.Parse(Console.ReadLine());
             d = int.Parse(Console.ReadLine());
 
-            diferenca = ((a * b) - (c * d));
+            diferenca = ((x * b) - (c * d));
 
             Console.WriteLine("DIFERENCA = " + diferenca);
 
