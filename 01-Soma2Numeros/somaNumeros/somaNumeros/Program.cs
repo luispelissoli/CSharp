b8f04f9a13b8cd0a21d75cdf66862fbacf0a2d35
Luis Vinicius Pelissoli <72233426+luispelissoli@users.noreply.github.com>
2021-03-25 13:25:47 -0300

    Estrutura sequencial

---
+++ 01-Soma2Numeros/somaNumeros/somaNumeros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace somaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+            mensagem explicativa */
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int soma = n1 + n2;
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
---
+++ 03-DiferencaProduto/DiferencaProduto/DiferencaProduto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DiferencaProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+            de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
+
+            int a, b, c, d, diferenca;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            diferenca = ((a * b) - (c * d));
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
+
+        }
+    }
+}
