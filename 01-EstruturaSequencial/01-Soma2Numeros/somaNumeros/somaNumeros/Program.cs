a207e22fca06dd28a982688348dc04cd0d596e8e
Luis Vinicius Pelissoli <72233426+luispelissoli@users.noreply.github.com>
2021-03-26 09:35:18 -0300

    Update Program.cs

--- 01-EstruturaSequencial/01-Soma2Numeros/somaNumeros/somaNumeros/Program.cs
+++ 01-EstruturaSequencial/01-Soma2Numeros/somaNumeros/somaNumeros/Program.cs
@@ -7,6 +7,7 @@ namespace somaNumeros
         static void Main(string[] args)
         {
 
+            
             /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
             mensagem explicativa */
 
